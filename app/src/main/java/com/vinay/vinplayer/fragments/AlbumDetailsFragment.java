0f92de606511b8afc78f08081a18e4b3843233b2
SALIMATTI\salimatti <venkatvinay6@gmail.com>
2017-02-04 03:04:22 +0530

    coordinate layout added

--- app/src/main/java/com/vinay/vinplayer/fragments/AlbumDetailsFragment.java
+++ app/src/main/java/com/vinay/vinplayer/fragments/AlbumDetailsFragment.java
@@ -1,17 +1,22 @@
 package com.vinay.vinplayer.fragments;
 
+import android.content.ContentUris;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.vinay.vinplayer.R;
 import com.vinay.vinplayer.adapters.AlbumSongsAdapter;
-import com.vinay.vinplayer.adapters.AllSongsAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,9 +45,24 @@ public class AlbumDetailsFragment extends DialogFragment {
 
         // Set the adapter
             Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.album_details_list);
 
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.album_details_list);
+            ImageView imageView= (ImageView) view.findViewById(R.id.iv_albumart);
+        Toolbar toolbar= (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitle(allsongs.get(0).get("album"));
+        toolbar.setSubtitle(allsongs.size()+"songs");
+        try {
+            final Uri sArtworkUri = Uri
+                    .parse("content://media/external/audio/albumart");
+            Uri uri = ContentUris.withAppendedId(sArtworkUri, Long.parseLong(allsongs.get(0).get("album_id")));
+            Picasso.with(context).load(uri).placeholder(R.drawable.albumart_default).error(R.drawable.albumart_default)
+                    .into(imageView);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
             recyclerView.setAdapter(new AlbumSongsAdapter(getContext(),allsongs, mListener));
         return view;
